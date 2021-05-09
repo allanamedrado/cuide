7e04dc0302bd840150ee6c2387755b72792ead5f
unknown <guifellim20@gmail.com>
2021-05-09 01:51:53 -0300

    pré-merge

--- cuide-front/routes.js
+++ cuide-front/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cuidadores = require('./src/app/controllers/cuidador')
+const consulta = require('./src/app/controllers/consulta')
 const routes = express.Router()
 
 
@@ -25,6 +26,7 @@ routes.get("/cuidadores/create", function(req, res) {
     return res.render("cuidadores/create")
 })
 
+routes.get("/consulta", consulta.index)
 routes.get("/cuidadores/index", cuidadores.index)
 routes.get("/cuidadores/create", cuidadores.create)
 routes.get("/cuidadores/:id", cuidadores.show)
